fc57a01c8acabab14d2e9ab7f4f75e8c2e3407a3
hajdaini <ajdaini.hatim@gmail.com>
2020-06-10 00:31:12 +0200

    modification du scheduler

--- src/food.py
+++ src/food.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3.6
 #coding:utf-8
+from . database import Database
 from . consumable import Consumable
 
 """
@@ -13,8 +14,17 @@ class Food(Consumable):
 		super().__init__(id, name, expiration)
 		self.heal = heal
 
+	"""
+	Enregistre un Food dans la base de données
+	"""
 	def create_food(self, db : Database):
 		pass
+
+	"""
+	Récupère un Food depuis la base de données
+	"""
+	@staticmethod
+	def get_food(db : Database, name : str):
 
 	@staticmethod
 	def get_food(db : Database, name : str):
--- src/scheduler.py
+++ src/scheduler.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.6
 #coding:utf-8
-from . database import Database
+from database import Database
 import csv
 
 """
@@ -16,38 +16,6 @@ class Scheduler:
 		Scheduler.update_database(db, foods)
 
 	@staticmethod
-	def load_from_csv_to_database(db : Database):
-		game_foods = Scheduler.get_data_from_file("../data/foods.csv")
-		column = {
-			"name": None, 
-			"expiration": None, 
-			"heal": None
-		}
-
-		foods = []
-
-		# TODO Adapter pour X colonnes
-		for row in game_foods:
-			column["name"] = row[0]
-			column["expiration"] = row[1]
-			column["heal"] = row[2]
-			foods.append(row)
-			db.save("foods", column)
-		
-		return foods
-
-	@staticmethod
-	def update_database(db : Database, foods : list):
-		rows = db.cursor.execute("SELECT * FROM foods")
-		database_foods = rows.fetchall()
-		i = 0
-		
-		for i in range(0, len(foods)):
-			if foods[i][1] != database_foods[i][2] or foods[i][2] != database_foods[i][3]:
-				sql = f"UPDATE foods SET expiration = \"{foods[i][1]}\", heal = \"{foods[i][2]}\" WHERE name = \"{foods[i][0]}\""
-				db.cursor.execute(sql)
-
-	@staticmethod
 	def get_data_from_file(filename):
 		with open(filename, "r") as f:
 			content = csv.reader(f, delimiter = ';')
@@ -61,7 +29,53 @@ class Scheduler:
 					datalist.append(row)
 
 		return datalist
-					
+
+	@staticmethod
+	def load_from_csv_to_database(db : Database):
+		game_foods = Scheduler.get_data_from_file("../data/foods.csv")
+		column = {
+			"name": None, 
+			"expiration": None, 
+			"heal": None
+		}
+
+		foods = []
+
+		# TODO Adapter pour X colonnes
+		for row in game_foods:
+			row = row[0].split(",")
+			column["name"] = row[0]
+			column["expiration"] = row[1]
+			column["heal"] = row[2]
+			foods.append(row)
+			db.save("foods", column)
+		
+		return foods
+
+	@staticmethod
+	def update_database(db : Database, foods : list):
+		rows = db.cursor.execute("SELECT * FROM foods")
+		database_foods = rows.fetchall()
+
+		# INSERT OR REPLACE INTO
+
+		for el in foods:
+			print(el)
+		print("#######")
+		for el in database_foods:
+			print(el)
+		
+		"""
+		i = 0
+		for i in range(0, len(foods)):
+			if not database_foods:
+				sql = f"INSERT INTO foods(name, expiration, heal) VALUES(\"{foods[i][0]}\", \"{foods[i][1]}\", \"{foods[i][2]}\")"
+			else:
+				if foods[i][1] != database_foods[i][2] or foods[i][2] != database_foods[i][3]:
+					sql = f"UPDATE foods SET expiration = \"{foods[i][1]}\", heal = \"{foods[i][2]}\" WHERE name = \"{foods[i][0]}\""
+			
+			db.cursor.execute(sql)
+		"""		
 
 if __name__ == '__main__':
-	Scheduler.get_data_from_file("../../data/foods.csv")
+	Scheduler.initialize(Database.GetInstance())
